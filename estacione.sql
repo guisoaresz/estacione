1a342aa0d850a6cb25619e3fcae52fa76577f22e
guisoaresz <guisxares@hotmail.com>
2024-05-12 17:20:48 -0300

    update database

--- estacione.sql
+++ estacione.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 06/05/2024 às 19:46
+-- Tempo de geração: 12/05/2024 às 22:20
 -- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+-- Versão do PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura para tabela `estacionamentos`
+--
+
+CREATE TABLE `estacionamentos` (
+  `idEstacionamento` int(11) NOT NULL,
+  `nomeEstacionamento` varchar(30) DEFAULT NULL,
+  `vagasEstacionamento` int(5) DEFAULT NULL,
+  `idProprietario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `estacionamentos`
+--
+
+INSERT INTO `estacionamentos` (`idEstacionamento`, `nomeEstacionamento`, `vagasEstacionamento`, `idProprietario`) VALUES
+(1, 'Estacionamento do Pedro', 10, 1);
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura para tabela `usuarios`
 --
 
@@ -31,19 +51,45 @@ CREATE TABLE `usuarios` (
   `idUsuario` int(11) NOT NULL,
   `userUsuario` varchar(25) NOT NULL,
   `emailUsuario` varchar(100) NOT NULL,
-  `senhaUsuario` varchar(25) NOT NULL
+  `senhaUsuario` varchar(255) NOT NULL,
+  `fotoUsuario` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`idUsuario`, `userUsuario`, `emailUsuario`, `senhaUsuario`) VALUES
-(1, 'admin', 'adm@gmail.com', '1234');
+INSERT INTO `usuarios` (`idUsuario`, `userUsuario`, `emailUsuario`, `senhaUsuario`, `fotoUsuario`) VALUES
+(1, 'admin', 'gui@hotmail.com', '$2y$10$EUy6WPASz1A9b6Wxg/T9aeXxm11uIdOrQMBsjfX7ebiPweEB5zpDC', 'no-image.png');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `vagas`
+--
+
+CREATE TABLE `vagas` (
+  `idVaga` int(11) NOT NULL,
+  `numeroVaga` int(11) DEFAULT NULL,
+  `idEstacionamento` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `vagas`
+--
+
+INSERT INTO `vagas` (`idVaga`, `numeroVaga`, `idEstacionamento`) VALUES
+(1, 12, 1);
 
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices de tabela `estacionamentos`
+--
+ALTER TABLE `estacionamentos`
+  ADD PRIMARY KEY (`idEstacionamento`);
 
 --
 -- Índices de tabela `usuarios`
@@ -52,14 +98,32 @@ ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`idUsuario`);
 
 --
+-- Índices de tabela `vagas`
+--
+ALTER TABLE `vagas`
+  ADD PRIMARY KEY (`idVaga`);
+
+--
 -- AUTO_INCREMENT para tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `estacionamentos`
+--
+ALTER TABLE `estacionamentos`
+  MODIFY `idEstacionamento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `vagas`
+--
+ALTER TABLE `vagas`
+  MODIFY `idVaga` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
